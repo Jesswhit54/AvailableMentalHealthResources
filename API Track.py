3c27ea4a8dbd6ba026470e7f7d7f72ed0a74cb28
Jesswhit54 <Jesswhit54@gmail.com>
2023-04-28 19:46:09 -0400

    merged data

--- API Track.py
+++ API Track.py
@@ -37,6 +37,18 @@ under_18_county["POP"]=under_18_county["POP"].astype(int)
 # import CSV file for Behavioral health service providers by county in 2015
 mental_health_2015 = pd.read_csv('Behavioral health service providers by county 2015.csv')
 
+
+under_18_county['NAME'] = under_18_county['NAME'].str.replace(' County, New York', '')
+
+under_18_county= under_18_county.drop(columns= 'state')
+under_18_county= under_18_county.drop(columns= 'county')
+
+under_18_county = under_18_county.rename(columns={"NAME": "County"})
+
 # merge data sets/ join data
+
+youth_MHS = under_18_county.merge(mental_health_2015, left_on=["County"], right_on= ["County"], how= 'left')
+
+
 
 
